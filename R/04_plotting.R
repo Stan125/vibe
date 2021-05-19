51a3f2649d8f3cbc38feab2f38a91769410a378f
Stanislaus Stadlmann <stani@Stanislauss-MacBook-Pro.local>
2021-05-19 10:48:01 +1000

    add option to have raw independent effects

--- R/04_plotting.R
+++ R/04_plotting.R
@@ -6,16 +6,34 @@
 #' @import ggplot2
 #' @export
 plot.vibe <- function(x, ...) {
-  gg <- ggplot(x$results, aes(x = var, y = indep_perc)) +
-    geom_bar(stat = "identity", position = position_dodge()) +
-    labs(x = "Variables", y = "%",
-         title = paste0("Percentages of independent effects on '",
-                        x$depvar_name,"'"),
-         subtitle = paste0("Modeled parameters: '",
-                           paste(unique(x$results$param), collapse = ", "), "'\n",
-                           "Used metric: '", x$metric, "'")) +
-    theme_bw() +
-    coord_flip()
+
+  if (!exists(perc))
+    perc <- FALSE
+
+  if (perc) {
+    gg <- ggplot(x$results, aes(x = var, y = indep_perc)) +
+      geom_bar(stat = "identity", position = position_dodge()) +
+      labs(x = "Variables", y = "%",
+           title = paste0("Percentages of independent effects on '",
+                          x$depvar_name,"'"),
+           subtitle = paste0("Modeled parameters: '",
+                             paste(unique(x$results$param), collapse = ", "), "'\n",
+                             "Used metric: '", x$metric, "'")) +
+      theme_bw() +
+      coord_flip()
+  } else {
+    gg <- ggplot(x$results, aes(x = var, y = indep_effects)) +
+      geom_bar(stat = "identity", position = position_dodge()) +
+      labs(x = "Variables", y = "%",
+           title = paste0("Percentages of independent effects on '",
+                          x$depvar_name,"'"),
+           subtitle = paste0("Modeled parameters: '",
+                             paste(unique(x$results$param), collapse = ", "), "'\n",
+                             "Used metric: '", x$metric, "'")) +
+      theme_bw() +
+      coord_flip()
+  }
+
 
   # if multiple pars
   if (x$npar == 2)
