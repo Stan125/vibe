52a5477682d6822261ca56d5625173c27e25048b
Stan125 <stanislaus@stadlmann.cm>
2023-11-07 15:23:23 +1100

    Rename R files

--- R/gof.R
+++ R/gof.R
@@ -1,3 +1,13 @@
+#' @title Function to obtain goodness-of-fit from regression object
+#' @description blabla
+#'
+#' @param object Regression object. One of \code{vibe:::supported_classes}.
+#' @param ... Additional arguments
+#' @export
+gof <- function(object, ...) {
+  UseMethod("gof", object)
+}
+
 #' @importFrom stats logLik
 #' @export
 #' @method gof default
@@ -16,12 +26,4 @@ gof.default <- function(object, gofmetric = "R2e", m0 = NULL, ...) {
     n <- length(object$y)
     return(1 - (lm / l0)^(-(2 / n) * l0))
   }
-}
-
-#' Function to obtain goodness-of-fit from regression object
-#' @param object Regression object. One of \code{vibe:::supported_classes}.
-#' @param ... Additional arguments
-#' @export
-gof <- function(object, ...) {
-  UseMethod("gof", object)
 }
--- data-raw/supported_classes.R
+++ data-raw/supported_classes.R
@@ -1,3 +1,5 @@
 ### This is the list of supported classes ###
 supported_classes <- c("glm", "gam", "gamlss")
+varimp_metrics <- c("hier.part", "relweights")
+gof_metrics <- c("")
 save(list = "supported_classes", file = "R/sysdata.rda")
