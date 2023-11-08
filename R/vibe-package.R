f4a8178c92fd0c0cacf2c580517f390355e992c6
Stan125 <stanislaus@stadlmann.cm>
2023-11-08 16:03:06 +1100

    Change where package documentation sits

---
+++ R/vibe-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' vibe: Variable Importance BEyond linear models
+#'
+#' This is a very cool package. So hot right now.
+#'
+#' @docType package
+#' @useDynLib vibe
+#' @importFrom Rcpp sourceCpp
+#' @name vibe
+NULL
--- R/vibe.R
+++ R/vibe.R
@@ -1,13 +1,3 @@
-#' vibe: Variable Importance BEyond linear models
-#'
-#' This is a very cool package. So hot right now.
-#'
-#' @docType package
-#' @useDynLib vibe
-#' @importFrom Rcpp sourceCpp
-#' @name vibe
-NULL
-
 #' Main vibe function
 #' @export
 #' @param object An object included in \code{vibe:::supported_classes}
