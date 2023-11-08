2c7221f32e0af355ae7ca954eaf57c48360e1ec4
Stan125 <stanislaus@stadlmann.cm>
2023-11-08 16:48:54 +1100

    Create supported classes table

--- data-raw/supported_classes.R
+++ data-raw/supported_classes.R
@@ -1,5 +1,7 @@
 ### This is the list of supported classes ###
-supported_classes <- c("glm", "gam", "gamlss")
-varimp_metrics <- c("hier.part", "relweights")
-gof_metrics <- c("")
+supported_classes <- c("lm", "glm", "gam", "gamlss")
+varimp_measure <- c("hier.part", "relweights")
+gof_metric <- c("R2e", "deviance", "R2", "log_lik")
+df <- expand.grid(supported_classes, varimp_measure, gof_metric)
+
 save(list = "supported_classes", file = "R/sysdata.rda")
