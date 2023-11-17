7acabffd645ccc8158ebb097f356e245ffe0476b
Stan125 <stanislaus@stadlmann.cm>
2023-11-17 16:54:04 +1100

    Only remove file if it exists

--- tests/testthat/test_99_cleanup.R
+++ tests/testthat/test_99_cleanup.R
@@ -1,4 +1,7 @@
 # ---- Cleanup script
-
-file.remove("vibe_plots.pdf")
-file.remove("models.RData")
+if (file.exists("vibe_plots.pdf")) {
+  file.remove("vibe_plots.pdf")
+}
+if (file.remove("models.RData")) {
+  file.remove("models.RData")
+}
